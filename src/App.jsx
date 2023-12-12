832d958990cf58efe67420217d6cb255cb3a83da
jushen dhillon <jushendhillon@London-Macbook.local>
2023-12-11 21:52:31 -0800

    renamed component with proper caps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Header from "./components/header";
+import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
 import ContactMe from "./components/ContactMe";

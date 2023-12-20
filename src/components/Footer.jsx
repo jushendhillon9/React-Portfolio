67f33b42155ecd7aa7e3bb05f5a23125b2b19d83
Jushen Dhillon <137123520+jushendhillon9@users.noreply.github.com>
2023-12-19 21:13:34 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,19 @@ function Footer () {
     return (
         <div id = "footerIconContainer">
             <div>
-                <img src = "/github_icon.png" className = "footerImage"></img>
+                <a target = "blank" href = "https://github.com/jushendhillon9">
+                    <img src = "/github_icon.png" className = "footerImage"></img>
+                </a>
             </div>
             <div>
-                <img src = "/linkedin_icon.png" className = "footerImage"></img>
+                <a target = "blank" href = "https://www.linkedin.com/in/jushendhillon/">
+                    <img src = "/linkedin_icon.png" className = "footerImage"></img>
+                </a>
             </div>
             <div>
-                <img src = "/devToLogo.svg" className = "footerImage"></img>
+                <a target = "blank" href = "https://dev.to/jushendhillon9">
+                    <img src = "/devToLogo.svg" className = "footerImage"></img>
+                </a>
             </div>
         </div>
     )
